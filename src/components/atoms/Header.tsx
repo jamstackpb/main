5599bef6d6e7d06573564449155992d27c051537
smate <smateusz49@interia.eu>
2021-08-06 10:17:49 +0200

    Wstawienie pełnego loga

--- src/components/atoms/Header.tsx
+++ src/components/atoms/Header.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import full_logo1 from '@/public/images/full_logo1.svg';
 
 export const Header = styled.div`
 	background:#15159b;
@@ -9,4 +10,14 @@ export const Header = styled.div`
 	display:flex;
 	justify-content:center;
 	align-items:center;
+`
+
+export const Full_Logo = styled.div`
+    width:168px;
+    height:50px;
+    margin:28px;
+    background-image: url(${full_logo1.src});
+    position: absolute;
+    top: 0;
+    left: 0;
 `
