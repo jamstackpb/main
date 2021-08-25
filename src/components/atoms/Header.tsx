44fca8a65cdbde06fd242f1745ce1de54d890aef
smate <smateusz49@interia.eu>
2021-08-25 16:03:36 +0200

    Header z kolorami jako zmiennymi color i naprawiony margin w body

--- src/components/atoms/Header.tsx
+++ src/components/atoms/Header.tsx
@@ -1,23 +1,26 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
 import full_logo1 from '@/public/images/full_logo1.svg';
+import { colors } from '@/src/styles';
+
+const prefix = '/main';
 
 export const Header = styled.div`
-	background:#15159b;
-	color: #fff;
-	font-size:18px;
-	padding:100px 40px;
-	width:100%;
-	display:flex;
-	justify-content:center;
-	align-items:center;
-`
+    background: ${colors.header_color};
+    color: ${colors.white};
+    font-size: 18px;
+    padding: 100px 40px;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
 
 export const Full_Logo = styled.div`
-    width:168px;
-    height:50px;
-    margin:28px;
-    background-image: url(${full_logo1.src});
+    width: 168px;
+    height: 50px;
+    margin: 28px;
+    background-image: url(${prefix + full_logo1.src});
     position: absolute;
     top: 0;
     left: 0;
-`
+`;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -36,7 +36,7 @@ export default class MyDocument extends Document {
             </>
           )} */}
                 </Head>
-                <body>
+                <body style={{ margin: 0 }}>
                     <Main />
                     <NextScript />
                 </body>
