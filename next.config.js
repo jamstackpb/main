009d8f7fc2e2a575c9cf3d8cf9495c3acaeed77b
smate <smateusz49@interia.eu>
2021-08-27 11:11:31 +0200

    rozwizane konflikty i wstawiony basePath i favicon (wersja div background) 2

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 const withPlugins = require('next-compose-plugins');
 
-module.exports = withPlugins([{ trailingSlash: true }]);
+module.exports = withPlugins([
+    {
+        trailingSlash: true,
+        basePath: process.env.NEXT_PUBLIC_BASE_PATH, //potrzebny
+        //assetPath: process.env.NEXT_PUBLIC_BASE_PATH, //nie dziala
+        devIndicators: {
+            autoPrerender: false,
+        },
+    },
+]);
