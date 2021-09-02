4d0461560c740f4d50e4c41e541466ce4b5c9605
Piotr Burzynski <elwis33333@gmail.com>
2021-09-02 14:01:08 +0200

    assets fix 2

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withPlugins = require('next-compose-plugins');
 module.exports = withPlugins([
     {
         trailingSlash: true,
+        assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH, //dziala
         devIndicators: {
             autoPrerender: false,
         },
