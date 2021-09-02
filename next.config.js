83be678314fd704d9a57c11a2334a7ec9cff1782
Piotr Burzyński <46093798+elwis33333@users.noreply.github.com>
2021-09-02 12:16:51 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withPlugins = require('next-compose-plugins');
 module.exports = withPlugins([
     {
         trailingSlash: true,
-        basePath: `${process.env.NEXT_PUBLIC_BASE_PATH}/main`, //potrzebny
+        basePath: process.env.NEXT_PUBLIC_BASE_PATH, //potrzebny
         assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH, //dziala
         devIndicators: {
             autoPrerender: false,
