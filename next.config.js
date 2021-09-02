cc4deb8ffd4446cff12a0030807ccbf55303e000
Piotr Burzynski <elwis33333@gmail.com>
2021-09-02 14:28:59 +0200

    assets fix 6

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 const withPlugins = require('next-compose-plugins');
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = withPlugins([
     {
         trailingSlash: true,
         basePath: process.env.NEXT_PUBLIC_BASE_PATH, //potrzebny
-        assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH, //dziala
+        assetPrefix: isProd ? 'https://jamstackpb.github.io/main' : '', //dziala
         devIndicators: {
             autoPrerender: false,
         },
