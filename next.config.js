7c0a5c67810d7dba65d0c4aa26f179b247ea06bb
Piotr Burzynski <elwis33333@gmail.com>
2021-09-03 12:12:33 +0200

    change test in next.js config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,25 @@
-const withPlugins = require('next-compose-plugins');
+// const withPlugins = require('next-compose-plugins');
 
 const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = withPlugins([
-    {
-        trailingSlash: true,
-        basePath: process.env.NEXT_PUBLIC_BASE_PATH, //potrzebny
-        //dziala czy niedziala, -> niepotrzebny
-        devIndicators: {
-            autoPrerender: false,
-        },
+// module.exports = withPlugins([
+//     {
+//         trailingSlash: true,
+//         basePath: process.env.NEXT_PUBLIC_BASE_PATH, //potrzebny
+//         //dziala czy niedziala, -> niepotrzebny
+//         devIndicators: {
+//             autoPrerender: false,
+//         },
+//     },
+//     { assetPrefix: isProd ? 'https://jamstackpb.github.io/main/' : '' },
+// ]);
+
+module.exports = {
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH, //potrzebny
+    //dziala czy niedziala, -> niepotrzebny
+    devIndicators: {
+        autoPrerender: false,
     },
-    { assetPrefix: isProd ? 'https://jamstackpb.github.io/main/' : '' },
-]);
+
+    assetPrefix: isProd ? 'https://jamstackpb.github.io/main/' : '',
+};
