e5333f7d6d81bc382c57e673bdc08434969cd598
Piotr Burzynski <elwis33333@gmail.com>
2021-09-03 11:43:39 +0200

    assets fix 19

--- next.config.js
+++ next.config.js
@@ -1,19 +1,14 @@
 const withPlugins = require('next-compose-plugins');
 
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = withPlugins([
     {
         trailingSlash: true,
         basePath: process.env.NEXT_PUBLIC_BASE_PATH, //potrzebny
-<<<<<<< HEAD
-        assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH, //dziala czy niedziala, -> niepotrzebny
-=======
-        assetPrefix: isProd ? '/main' : '', //dziala czy niedziala, -> niepotrzebny
->>>>>>> f726f553b6d89a367934ce7d6d2345f2b63a9ee7
+        assetPrefix: isProd ? 'https://jamstackpb.github.io/main/' : '', //dziala czy niedziala, -> niepotrzebny
         devIndicators: {
             autoPrerender: false,
-        },
-        images: {
-            path: `${process.env.NEXT_PUBLIC_BASE_PATH}/_next/image`,
         },
     },
 ]);
