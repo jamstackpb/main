01307ddeab7b480a17f59db1963103dab791d762
Your Name <you@example.com>
2021-11-02 15:59:40 +0100

    Zaktualizowany opis

--- src/pages/aboutUs.tsx
+++ src/pages/aboutUs.tsx
@@ -3,6 +3,7 @@ import { Header } from '@/src/components/molecules/Header';
 import { CopyRights } from '../components/molecules/CopyRights';
 import { InferGetStaticPropsType } from 'next';
 import styled from '@emotion/styled';
+import Link from 'next/link';
 
 const A1 = styled.h1`
     padding: 4rem 0 2rem;
@@ -10,7 +11,7 @@ const A1 = styled.h1`
 
 const B1 = styled.h2`
     color: rgb(138, 51, 51);
-    padding: 2rem 0 6rem;
+    padding: 2rem 8rem 6rem;
 `;
 
 const about = ({ about }: InferGetStaticPropsType<typeof getStaticProps>) => {
@@ -22,24 +23,49 @@ const about = ({ about }: InferGetStaticPropsType<typeof getStaticProps>) => {
             </A1>
             <B1>
                 <h2 className="text-2xl text-center mb-10">
-                    Jesteśmy kołem naukowym <b>Jamstack</b>! Zajmujemy się tworzeniem frontendu, czyli tworzeniem stron
-                    internetowych od strony wizualnej. Korzystamy z wielu nowych i innowacyjnych technologii, które
-                    pozwalają nam budować zaawansowane projekty z dużą łatwością i efektywnością. Celujemy w jak
-                    największą prostotę wizualną i funkcjonalną naszych prac.
+                    <div>
+                        Jesteśmy kołem naukowym{' '}
+                        <Link href="/">
+                            <a>
+                                <b>Jamstack</b>
+                            </a>
+                        </Link>
+                        ! Zajmujemy się tworzeniem frontendu, czyli tworzeniem stron internetowych od strony wizualnej.
+                        Korzystamy z wielu nowych i innowacyjnych technologii, które pozwalają nam budować zaawansowane
+                        projekty z dużą łatwością i efektywnością. Celujemy w jak największą prostotę wizualną i
+                        funkcjonalną naszych prac.
+                    </div>
+                    <br />
+                    <div>
+                        Praca nad projektami jest wygodna i w pełni zdalna, gdyż większość spotkań odbywa się przez{' '}
+                        <a href="https://discord.gg/CQpKMTNXMt">
+                            <b>Discord</b>
+                        </a>
+                        , a modyfikowanie i budowanie projektów przebiega przez{' '}
+                        <a href="https://github.com/jamstackpb">
+                            <b>Github</b>
+                        </a>
+                        .
+                    </div>
+                    <br />
+                    <div>
+                        Rozwijanie projektów wymaga kreatywności, odrobiny zmysłu artystycznego i estetycznego, jako iż
+                        budujemy strony internetowe, które chcemy, by były jak najlepsze i jak najczytelniejsze. Zawsze
+                        szukamy nowych rozwiązań, które pomagają nam przyśpieszyć i ułatwić pracę.
+                    </div>
+                    <br />
+                    <div>
+                        Pomimo iż jesteśmy kółkiem na wydziale mechanicznym, przyjmujemy osoby z innych wydziałów, które
+                        zainteresowane są pracowaniem we front-endzie. Dla nowych członków zawsze są miejsca wolne!
+                        Zachęcamy do dołączenia i powiększenia naszego grona programistów!
+                    </div>
                 </h2>
-                <img src="https://jooinn.com/images/under-construction-3.png" width={100} />
             </B1>
             <CopyRights />
         </Layout>
     );
 };
 /*
--Jesteśmy kółkiem naukowym wielowydziałowym (czy cos)
--Ilosc czlonow
--mamy miejsca dla osob chcacych nauczyc sie frontendu
--Szukamy nowych rozwiazan
--praca glownie zdalna, dzieki githubowi
--Praca jest wygodna
--Praca wymaga kreatywnosci i zmyslu artystycznego, i estetycznego
+-Praca wymaga kreatywnosci i zmyslu artystycznego, i estetycznego, co skłania nas do Szukania nowych rozwiazan
 */
 export default about;
