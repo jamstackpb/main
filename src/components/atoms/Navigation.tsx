c6d161f97db1c871fb33f5b5ecf28fb5168b4bb6
ghosttly <ghosttlyg@gmail.com>
2021-11-05 21:41:57 +0100

    fixed dropmenu top position

--- src/components/atoms/Navigation.tsx
+++ src/components/atoms/Navigation.tsx
@@ -1,13 +1,10 @@
 import { Logo, Menu } from '@/src/components';
 import { useState } from 'react';
 
-export const Navigation: React.FC<{ isHomepage?: boolean }> = ({ isHomepage = false }) => {
-    const [clicked, setClicked] = useState(false);
-    const ToggleMenu = () => setClicked(!clicked);
+export const Navigation: React.FC = () => {
     return (
         <div className="w-full bg-primaryBackgroundColor text-base max-h-22 h-22 z-50 fixed top-0 flex justify-between ">
             <Menu />
-
             <Logo />
         </div>
     );
