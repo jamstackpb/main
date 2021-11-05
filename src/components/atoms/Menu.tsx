164f77431c3dfe99ce065091d06d0bec6c98632d
ghosttly <ghosttlyg@gmail.com>
2021-11-05 21:19:11 +0100

    quick fix dropmenu hide move duration

--- src/components/atoms/Menu.tsx
+++ src/components/atoms/Menu.tsx
@@ -48,7 +48,9 @@ export const Menu: React.FC = () => {
             <MenuIcon clicked={clicked} click={ToggleMenu} />
             <DropMenu
                 className={`bg-gray-900 text-white flex flex-col items-center justify-center fixed top-0 w-full h-full z-auto md:w-60 ${
-                    clicked ? 'transform duration-500 translate-x-0' : ' transform ease-in -translate-x-full'
+                    clicked
+                        ? 'transform duration-500 translate-x-0'
+                        : ' transform ease-in duration-300 -translate-x-full'
                 }`}
             >
                 {content.map(({ link, name }, i) => (
